b5799c1dbefc1e33f7c26481c1ea2df19bb716cc
Sarthak Singh Rawat <155871591+Sarthakrawt@users.noreply.github.com>
2024-10-11 12:46:53 +0530

    Create App.js

---
+++ reactbasicapp/App.js
@@ -0,0 +1,11 @@
+// this is simple fuction component in react-app 
+// and in package-json file is for check the dependencies , scripts, etc 
+// package.json file consist of file which you installed and many more 
+function App() {
+  return (
+   <>hello world</>
+  );
+}
+
+// 
+export default App;
