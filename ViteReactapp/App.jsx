9037ba23e6d8ac00e5b1c4efd2bf5aaf977591dd
Sarthak Singh Rawat <155871591+Sarthakrawt@users.noreply.github.com>
2024-10-15 06:58:52 +0530

    Create App.jsx

---
+++ ViteReactapp/App.jsx
@@ -0,0 +1,16 @@
+ import Chai from "./chai";
+function App() {
+ // react file save as .jsx extension 
+const username = "chai aur code"
+// you can only return one tag 
+  return (
+    // to make every time div you can also make empty container called fragment 
+    <>
+        <Chai/>
+        <p>chai pe lo friends{username}</p>
+    </>
+ 
+  )
+}
+
+export default App
